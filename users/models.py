8a173643ef97fcf542e202e7e9b3d52e4321aba7
Yogesh Anil Shirsath <76992562+Yogeshshirsath01@users.noreply.github.com>
2021-03-31 23:50:06 +0530

    Add files via upload

---
+++ users/models.py
@@ -0,0 +1,17 @@
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Profile(models.Model):
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    website = models.URLField(max_length=200, blank=True)
+    biography = models.TextField(blank=True)
+    phone_number = models.CharField(max_length=20, blank=True)
+    picture = CloudinaryField(default='')
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.username

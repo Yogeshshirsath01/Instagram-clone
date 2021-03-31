fcbf292af9adf39362ff98ebb6d1d55c2035363b
Yogesh Anil Shirsath <76992562+Yogeshshirsath01@users.noreply.github.com>
2021-03-31 23:53:57 +0530

    Add files via upload

---
+++ posts/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from users.models import Profile
+from cloudinary.models import CloudinaryField
+
+
+class Post(models.Model):
+
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+    title = models.CharField(max_length=255)
+    photo = CloudinaryField(default='')
+
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """Return title and username"""
+        return "{} by @{}".format(self.title, self.profile.user.username)
+
+
+# class User(models.Model):
+#     """User model."""
+
+#     email = models.EmailField(unique=True)
+#     password = models.CharField(max_length=100)
+
+#     first_name = models.CharField(max_length=100)
+#     last_name = models.CharField(max_length=100)
+
+#     is_admin = models.BooleanField(default=False)
+
+#     bio = models.TextField(blank=True)
+
+#     birthdate = models.DateField(blank=True, null=True)
+
+#     created = models.DateTimeField(auto_now_add=True)
+#     modified = models.DateTimeField(auto_now=True)
+
+#     def __str__(self):
+#         """Return email."""
+#         return self.email
